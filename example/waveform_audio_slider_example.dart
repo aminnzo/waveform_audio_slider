dba1108e187893c06a4bbeabb426b47541e7f692
enzo <dartmolecules@gmail.com>
2023-11-07 15:47:49 +0330

    create package

---
+++ example/waveform_audio_slider_example.dart
@@ -0,0 +1,6 @@
+import 'package:waveform_audio_slider/waveform_audio_slider.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/waveform_audio_slider.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/waveform_audio_slider_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
