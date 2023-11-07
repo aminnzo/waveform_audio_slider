872c45ada2a10f0695caa3ced1612b7e665d2bd8
enzo <dartmolecules@gmail.com>
2023-11-07 17:09:08 +0330

    export used classes and update .gitignore

--- lib/waveform_audio_slider.dart
+++ lib/waveform_audio_slider.dart
@@ -1,3 +1,6 @@
 library waveform_audio_slider;
 
 export 'src/waveform_slider.dart';
+export 'src/data/scaling_algorithm_type.dart';
+export 'src/data/waveform_style.dart';
+export 'src/data/audio_wave_source.dart';
